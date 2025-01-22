9d149a1e19f33a00c0f4e3ce5081789f37ddf185
YanGraim <yangraim@gmail.com>
2025-01-21 22:08:48 -0300

    adiciona useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,37 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [nome, setNome] = useState("");
+  const [idade, setIdade] = useState("");
+
+  function handleCalcular() {
+    alert("teste")
+  }
 
   return (
     <div className='container'>
       <h1 className='title'>Descubra a sua idade</h1>
 
-      <form className='form'>
+      <form className='form' onSubmit={handleCalcular}>
         <label>Informe seu nome</label>
-        <input type="text" placeholder='Digite seu nome'/>
+        <input 
+          className='input' 
+          type="text" 
+          placeholder='Digite seu nome'
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
 
         <label>Informe o ano que você nasceu</label>
-        <input type="text" placeholder='Digite o ano que nasceu' />
+        <input 
+          className='input' 
+          type="text" 
+          placeholder='Digite o ano que nasceu'
+          value={idade}
+          onChange={(e) => setIdade(e.target.value)}
+        />
 
-        <button>Descobrir idade</button>
+        <input className='button' type='submit' value='Descobrir idade'/>
       </form>
     </div>
   )
